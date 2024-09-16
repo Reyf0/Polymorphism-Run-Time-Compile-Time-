8e2a7e33edab4981f0126cf282ebc0e4d06f7d84
Nikita <extremeprogrammingray@gmail.com>
2024-09-16 16:51:31 +0700

    Добавьте файлы проекта.

---
+++ Polymorphism 1z/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_1z
+{
+    public static class CustomConverter
+    {
+        public static void Convert(string a, out int b)
+        {
+            b = System.Convert.ToInt32(a);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string abc = "123";
+            int n;
+            CustomConverter.Convert(abc, out n);
+            Console.WriteLine(n);
+        }
+    }
+}
